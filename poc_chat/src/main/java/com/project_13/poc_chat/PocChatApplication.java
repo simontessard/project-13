b5e7668e35951f9e0c40a87a39a76dbf3fb14a24
simontessard <simon.tessard@gmail.com>
2024-08-19 17:20:32 +0200

    Initial commit

---
+++ poc_chat/src/main/java/com/project_13/poc_chat/PocChatApplication.java
@@ -0,0 +1,13 @@
+package com.project_13.poc_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocChatApplication.class, args);
+	}
+
+}
---
+++ poc_chat/src/test/java/com/project_13/poc_chat/PocChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_13.poc_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
