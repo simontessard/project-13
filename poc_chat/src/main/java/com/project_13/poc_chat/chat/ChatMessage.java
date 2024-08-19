3bb56d433a8e2a07867f4de5bc7cb685fe053d0d
simontessard <simon.tessard@gmail.com>
2024-08-19 19:11:11 +0200

    Chat integration

---
+++ poc_chat/src/main/java/com/project_13/poc_chat/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.project_13.poc_chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
---
+++ poc_chat/src/main/java/com/project_13/poc_chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.project_13.poc_chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
